3175e5cca214a8d3251445ef53bd1081156a4408
Daniel Arbelo <alu0101117621@ull.edu.es>
2022-04-14 21:25:22 +0000

    a

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,78 @@ enum Comands {
     addElemento = "Añadir un género, canción, album, grupo u artista",
     rmElemento = "Eliminar un género, canción, album, grupo u artista",
     modifyElemento = "Modificar un género, canción, album, grupo u artista",
-     
+    imprimirCancionArtista = "Imprimir canciones pertenecientes a un artista",
+    imprimirCancionGrupo = "Imprimir canciones pertenecientes a un grupo",
+    imprimirAlbumArtista = "Imprimir Albumes pertenecientes a un artista",
+    imprimirAlbumGrupo = "Imprimir Albumes pertenecientes a un grupo",
+    imprimirPlaylistArtista = "Imprimir Playlists pertenecientes a un artista",
+    imprimirPlaylistGrupo = "Imprimir Playlists pertenecientes a un grupo",
+    imprimirCancionAlfabeticamente = "Imprimir canciones alfabeticametne",
+    imprimirAlbumAlfabeticamente = "Imprimir albumes alfabeticametne",
+    imprimirPlaylistAlfabeticamente = "Imprimir canciones alfabeticametne",
+    imprimirAlbumPorAño = "Imprimir album por año de lanzamiento",
+    imprimirCancionesReproducciones = "Imprimir canciones por numerod e reproducciones",
+    imprimirSingles = "Imprimir únicamente las canciones que son singles",
     Quit = "Salir"
+}
+
+async function promptUser() {
+    console.clear();
+
+    const answers = await inquirer.prompt({
+        type: 'list',
+        name: 'comand',
+        message: 'Elige una opción', 
+        choises: Object.values(Comands)
+    });
+
+    switch (answers["comand"]) {
+        case Comands.addElemento:
+            
+            break;
+        case Comands.rmElemento:
+            
+            break;
+        case Comands.modifyElemento:
+            
+            break;
+        case Comands.imprimirCancionArtista:
+            
+            break;
+        case Comands.imprimirCancionGrupo:
+            
+            break;
+        case Comands.imprimirAlbumArtista:
+            
+            break;
+        case Comands.imprimirAlbumGrupo:
+            
+            break;
+        case Comands.imprimirPlaylistArtista:
+            
+            break;
+        case Comands.imprimirPlaylistGrupo:
+            
+            break;
+        case Comands.imprimirCancionAlfabeticamente:
+            
+            break;
+        case Comands.imprimirAlbumAlfabeticamente:
+            
+            break;
+        case Comands.imprimirPlaylistAlfabeticamente:
+            
+            break;
+        case Comands.imprimirAlbumPorAño:
+            
+            break;
+        case Comands.imprimirCancionesReproducciones:
+            
+            break;  
+        case Comands.imprimirSingles:
+            
+            break;   
+        default:
+            break;
+    }
 }
